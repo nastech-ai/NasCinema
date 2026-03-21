a3ec04a8819bdb8ce1792fb1a4bc92e9968416d2
dotcomnerd <70242273+dotcomnerd@users.noreply.github.com>
2026-03-21 07:10:33 -0700

    📦 deps + next/tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,9 @@ export default {
       },
     },
     extend: {
+      aspectRatio: {
+        poster: "2 / 3",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
