b39c9cea5d7befdd0502d988c5a1d7e95e0433d4
dotcomnerd <70242273+dotcomnerd@users.noreply.github.com>
2026-03-22 00:10:54 -0700

    🎪 tvshow shell, seasons graph

--- components/tvshow/series-graph/series-graph.tsx
+++ components/tvshow/series-graph/series-graph.tsx
@@ -21,6 +21,7 @@ const CanvasView = dynamic(
   { ssr: false },
 );
 
+// Threshold to determine if the series is large enough to use the canvas view. (Episodes)
 const LARGE_SERIES_THRESHOLD = 75;
 
 type SeriesGraphProps = {
